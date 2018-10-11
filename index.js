45a1bcd101b8fc2ed54ad089008dc303a12cdc80
severus889700 <severus889700@gmail.com>
2018-10-11 07:54:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -59,18 +59,7 @@ function createRock(x) {
 
 /*End the game by clearing `gameInterval`, removing all ROCKS from the DOM, and removing the `moveDodger` event listener.
      Finally, alert "YOU LOSE!" to the player.*/
-/*function endGame() {
-  while(ROCKS.length > 0){
-    GAME.removeChild(ROCKS[0]);
-    ROCKS.shift();
-  }
 
-  clearInterval(gameInterval);
-  window.removeEventListener('keydown', moveDodger);
-  alert("YOU LOSE!");
-  START.innerHTML = 'Play again?'
-  START.style.display = 'inline'
-}*/
 function endGame() {
   clearInterval(gameInterval);
   ROCKS.forEach(function(rock)
