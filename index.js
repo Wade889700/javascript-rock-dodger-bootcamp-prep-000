e214c1b8cdf174669180f4e6ae30bad1836b226d
severus889700 <severus889700@gmail.com>
2018-10-11 07:55:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -61,14 +61,17 @@ function createRock(x) {
      Finally, alert "YOU LOSE!" to the player.*/
 
 function endGame() {
-  clearInterval(gameInterval);
-  ROCKS.forEach(function(rock)
-      {rock.remove()
-      });
-  document.removeEventListener('keydown',moveDodger);
-  return alert('YOU LOSE!');
-}
+  while(ROCKS.length > 0){
+    GAME.removeChild(ROCKS[0]);
+    ROCKS.shift();
+  }
 
+  clearInterval(gameInterval);
+  window.removeEventListener('keydown', moveDodger);
+  alert("YOU LOSE!");
+  START.innerHTML = 'Play again?'
+  START.style.display = 'inline'
+}
 
 function moveDodger(e) {
   if(e.which == LEFT_ARROW){
